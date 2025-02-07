c3a93e338e889d606bfd5b78193c912d9279f223
rkemmey <rachel.kemmey@gmail.com>
2025-02-06 20:41:54 -0600

    updated

--- factorial.py
+++ factorial.py
@@ -1,3 +1,16 @@
 def factorial(num):
 	# your code here
-	pass
+	if num < 0:
+		return "Num must be positive"
+	if not isinstance(num, int):
+		return "Num must be an integer"
+	if num == 0:
+		return 1
+	else:
+		product = 1
+		for i in range(num, 1, -1):
+			#print(i)
+			product = product * i
+		return product
+		
+
